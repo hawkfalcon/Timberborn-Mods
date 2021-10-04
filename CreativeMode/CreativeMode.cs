822f98d99fdcb28ee1162ed4a73bef48abb12788
hawkfalcon <hello@tristenmiller.me>
2021-10-03 21:57:43 -0700

    First Commit of Creative Mode plugin for Timberborn

---
+++ CreativeMode/CreativeMode.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using HarmonyLib;
+using Timberborn.ScienceSystem;
+using Timberborn.BuildingTools;
+using Timberborn.BlockSystem;
+using Timberborn.Buildings;
+
+namespace CreativeModePlugin {
+    [BepInPlugin("com.hawkfalcon.plugin.creativemode", "Creative Mode", "1.0.0.0")]
+    public class CreativeModePlugin : BaseUnityPlugin {
+        public void OnEnable() {
+            var harmony = new Harmony("com.hawkfalcon.plugin.creativemode");
+            harmony.PatchAll();
+            Logger.LogInfo("Plugin Creative Mode is loaded!");
+        }
+    }
+
+    [HarmonyPatch(typeof(BuildingUnlockingService), "Unlocked")]
+    class PatchScience {
+        static bool Prefix(ref bool __result) {
+            __result = true;
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(BuildingPlacer), "Place")]
+    class PatchPlace {
+        static bool Prefix(BlockObject prefab) {
+            Building component = prefab.GetComponent<Building>();
+            component.PlaceFinished = true;
+            return true;
+        }
+    }
+}
---
+++ CreativeMode/package/README.md
@@ -0,0 +1,5 @@
+This mod lets you place anything, anytime
+- Nothing costs science 
+- Buildings are built instantly
+
+More to come
