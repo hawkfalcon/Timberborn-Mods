ddf2e937df98d11e216083c0473465c6583b73a8
hawkfalcon <hello@tristenmiller.me>
2021-10-04 21:26:02 -0700

    Add Config, move patches into main class

--- CreativeMode/CreativeMode.cs
+++ CreativeMode/CreativeMode.cs
@@ -1,4 +1,5 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using HarmonyLib;
 using Timberborn.ScienceSystem;
 using Timberborn.BuildingTools;
@@ -6,28 +7,47 @@ using Timberborn.BlockSystem;
 using Timberborn.Buildings;
 
 namespace CreativeModePlugin {
+
     [BepInPlugin("com.hawkfalcon.plugin.creativemode", "Creative Mode", "1.0.0.0")]
+    [HarmonyPatch]
     public class CreativeModePlugin : BaseUnityPlugin {
-        public void OnEnable() {
+        private static ConfigEntry<bool> disableScienceCost;
+        private static ConfigEntry<bool> enableInstantBuilding;
+
+        public void OnEnable()
+        {
+            disableScienceCost = Config.Bind("General.Features",
+               "DisableScienceCost", true, "Placing anything no longer requires science");
+            enableInstantBuilding = Config.Bind("General.Features",
+               "EnableInstantBuilding", true, "Anything that is placed is instantly built at no cost");
+
             var harmony = new Harmony("com.hawkfalcon.plugin.creativemode");
             harmony.PatchAll();
             Logger.LogInfo("Plugin Creative Mode is loaded!");
         }
-    }
 
-    [HarmonyPatch(typeof(BuildingUnlockingService), "Unlocked")]
-    class PatchScience {
-        static bool Prefix(ref bool __result) {
-            __result = true;
-            return false;
+        /*
+         * Disables Science Costs
+         */
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(BuildingUnlockingService), "Unlocked")]
+        static bool BuildingUnlocker(ref bool __result) {
+            if (disableScienceCost.Value) {
+                 __result = true;
+            }
+            return !disableScienceCost.Value;
         }
-    }
 
-    [HarmonyPatch(typeof(BuildingPlacer), "Place")]
-    class PatchPlace {
-        static bool Prefix(BlockObject prefab) {
-            Building component = prefab.GetComponent<Building>();
-            component.PlaceFinished = true;
+        /*
+         * Enables instant building
+         */
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(BuildingPlacer), "Place")]
+        static bool PlaceInstantly(BlockObject prefab) {
+            if (enableInstantBuilding.Value) {
+                Building component = prefab.GetComponent<Building>();
+                component.PlaceFinished = true;
+            }
             return true;
         }
     }
