5dd54e756afda0a53b1381cd2accf5eda8e0050f
hawkfalcon <hello@tristenmiller.me>
2021-10-05 17:37:42 -0700

    Unlock all faction uniques

--- CreativeMode/CreativeMode.cs
+++ CreativeMode/CreativeMode.cs
@@ -5,6 +5,13 @@ using Timberborn.ScienceSystem;
 using Timberborn.BuildingTools;
 using Timberborn.BlockSystem;
 using Timberborn.Buildings;
+using Timberborn.FactionSystemGame;
+using Timberborn.FactionSystem;
+using Timberborn.AssetSystem;
+using Timberborn.NeedSystem;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 namespace CreativeModePlugin {
 
@@ -13,13 +20,15 @@ namespace CreativeModePlugin {
     public class CreativeModePlugin : BaseUnityPlugin {
         private static ConfigEntry<bool> disableScienceCost;
         private static ConfigEntry<bool> enableInstantBuilding;
+        private static ConfigEntry<bool> enableAllFactionBuildings;
 
-        public void OnEnable()
-        {
+        public void OnEnable() {
             disableScienceCost = Config.Bind("General.Features",
                "DisableScienceCost", true, "Placing anything no longer requires science");
             enableInstantBuilding = Config.Bind("General.Features",
                "EnableInstantBuilding", true, "Anything that is placed is instantly built at no cost");
+            enableAllFactionBuildings = Config.Bind("General.Features",
+               "EnableAllFactionBuildings", true, "Unlocks access to ALL faction buildings");
 
             var harmony = new Harmony("com.hawkfalcon.plugin.creativemode");
             harmony.PatchAll();
@@ -32,10 +41,11 @@ namespace CreativeModePlugin {
         [HarmonyPrefix]
         [HarmonyPatch(typeof(BuildingUnlockingService), "Unlocked")]
         static bool BuildingUnlocker(ref bool __result) {
-            if (disableScienceCost.Value) {
-                 __result = true;
+            if (!disableScienceCost.Value) {
+                return true;
             }
-            return !disableScienceCost.Value;
+            __result = true;
+            return false;
         }
 
         /*
@@ -49,6 +59,49 @@ namespace CreativeModePlugin {
                 component.PlaceFinished = true;
             }
             return true;
+        }
+
+        /*
+         * Unlock all faction uniques
+         */
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(FactionObjectCollection), "GetObjects")]
+        static bool UnlockFactionUniques(FactionService ____factionService, IResourceAssetLoader ____resourceAssetLoader, ref IEnumerable<Object> __result) {
+            if (!enableAllFactionBuildings.Value) {
+                return true;
+            }
+            List<FactionSpecification> factions = Traverse.Create(____factionService)
+                .Field("_factionSpecificationService").Field("_factions").GetValue() as List<FactionSpecification>;
+
+            List<string> buildings = new();
+            buildings.AddRange(____factionService.Current.CommonBuildings);
+            foreach (FactionSpecification factionSpecification in factions) {
+                buildings.AddRange(factionSpecification.UniqueBuildings);
+            }
+            __result = (from path in buildings select ____resourceAssetLoader.Load<GameObject>(path));
+            return false;
+        }
+
+        /**
+         * If everything is unique, nothing is unique
+         */
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(UniqueBuildingCollection), "IsUnique")]
+        static bool UnlockAllUniques(ref bool __result) {
+            if (!enableAllFactionBuildings.Value) {
+                return true;
+            }
+            __result = false;
+            return false;
+        }
+
+        /**
+         * Disable needs
+         */
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(NeedManager), "Tick")]
+        static bool DisableNeed() {
+            return false;
         }
     }
 }
