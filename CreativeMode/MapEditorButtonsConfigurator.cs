20d04838fabd600b642aae89912849448a3458b2
hawkfalcon <hello@tristenmiller.me>
2021-10-17 17:01:13 -0700

    Split out classes

---
+++ CreativeMode/MapEditorButtonsConfigurator.cs
@@ -0,0 +1,73 @@
+﻿using Timberborn.MapEditor;
+using Timberborn.BottomBarSystem;
+using Timberborn.ToolPanelSystem;
+using Bindito.Core;
+
+namespace CreativeMode {
+    /**
+         * Modified from MapEditorConfigurator
+         */
+    public class MapEditorButtonsConfigurator : IConfigurator {
+
+        private class ToolPanelModuleProvider : IProvider<ToolPanelModule> {
+            private readonly NaturalResourceSpawningBrushPanel _naturalResourceSpawningBrushPanel;
+
+            private readonly BrushHeightPanel _brushHeightPanel;
+
+            private readonly BrushSizePanel _brushSizePanel;
+
+            private readonly BrushShapePanel _brushShapePanel;
+
+            public ToolPanelModuleProvider(BrushHeightPanel brushHeightPanel, BrushSizePanel brushSizePanel, BrushShapePanel brushShapePanel, NaturalResourceSpawningBrushPanel naturalResourceSpawningBrushPanel) {
+                _brushHeightPanel = brushHeightPanel;
+                _brushSizePanel = brushSizePanel;
+                _brushShapePanel = brushShapePanel;
+                _naturalResourceSpawningBrushPanel = naturalResourceSpawningBrushPanel;
+            }
+
+            public ToolPanelModule Get() {
+                ToolPanelModule.Builder builder = new ToolPanelModule.Builder();
+                builder.AddFragment(_naturalResourceSpawningBrushPanel, 5);
+                builder.AddFragment(_brushHeightPanel, 6);
+                builder.AddFragment(_brushSizePanel, 7);
+                builder.AddFragment(_brushShapePanel, 8);
+                return builder.Build();
+            }
+        }
+
+        private class BottomBarModuleProvider : IProvider<BottomBarModule> {
+            private readonly MapEditorGroupedButtons _mapEditorGroupedButtons;
+
+            public BottomBarModuleProvider(MapEditorGroupedButtons mapEditorGroupButtons) {
+                _mapEditorGroupedButtons = mapEditorGroupButtons;
+            }
+
+            public BottomBarModule Get() {
+                BottomBarModule.Builder builder = new BottomBarModule.Builder();
+                builder.AddMiddleSectionElements(_mapEditorGroupedButtons);
+                return builder.Build();
+            }
+        }
+
+        public void Configure(IContainerDefinition containerDefinition) {
+            containerDefinition.Bind<NaturalResourceLayerService>().AsSingleton();
+            containerDefinition.Bind<BrushProbabilityMap>().AsSingleton();
+            containerDefinition.Bind<BrushShapeIterator>().AsSingleton();
+            containerDefinition.Bind<NaturalResourceSpawner>().AsSingleton();
+            containerDefinition.Bind<NoStartingLocationAlertFragment>().AsSingleton();
+            containerDefinition.Bind<BrushHeightPanel>().AsSingleton();
+            containerDefinition.Bind<BrushSizePanel>().AsSingleton();
+            containerDefinition.Bind<BrushShapePanel>().AsSingleton();
+            containerDefinition.Bind<NaturalResourceSpawningBrushPanel>().AsSingleton();
+            containerDefinition.Bind<AbsoluteTerrainHeightBrushTool>().AsSingleton();
+            containerDefinition.Bind<RelativeTerrainHeightBrushTool>().AsSingleton();
+            containerDefinition.Bind<NaturalResourceSpawningBrushTool>().AsSingleton();
+            containerDefinition.Bind<NaturalResourceRemovalBrushTool>().AsSingleton();
+            containerDefinition.Bind<MapEditorButtons>().AsSingleton();
+            containerDefinition.Bind<NaturalResourceLayerToggle>().AsSingleton();
+            containerDefinition.MultiBind<ToolPanelModule>().ToProvider<ToolPanelModuleProvider>().AsSingleton();
+            containerDefinition.MultiBind<BottomBarModule>().ToProvider<BottomBarModuleProvider>().AsSingleton();
+        }
+
+    }
+}
---
+++ CreativeMode/MapEditorGroupedButtons.cs
@@ -0,0 +1,49 @@
+﻿using Timberborn.MapEditor;
+using Timberborn.BottomBarSystem;
+using Timberborn.ToolSystem;
+using System.Collections.Generic;
+
+namespace CreativeMode {
+
+    /**
+     * Modified from MapEditorButtons
+     */
+    public class MapEditorGroupedButtons : IBottomBarElementsProvider {
+        private static readonly string AbsoluteTerrainHeightBrushImageKey = "AbsoluteTerrainHeightBrush";
+
+        private static readonly string RelativeTerrainHeightBrushImageKey = "RelativeTerrainHeightBrushIcon";
+
+        private static readonly string NaturalResourcesSpawningImageKey = "NaturalResourcesIcon";
+
+        private static readonly string NaturalResourcesRemovalImageKey = "RemoveNaturalResourcesIcon";
+
+        private readonly ToolButtonFactory _toolButtonFactory;
+
+        private readonly AbsoluteTerrainHeightBrushTool _absoluteTerrainHeightBrushTool;
+
+        private readonly RelativeTerrainHeightBrushTool _relativeTerrainHeightBrushTool;
+
+        private readonly NaturalResourceSpawningBrushTool _naturalResourceSpawningBrushTool;
+
+        private readonly NaturalResourceRemovalBrushTool _naturalResourceRemovalBrushTool;
+
+        public MapEditorGroupedButtons(ToolButtonFactory toolButtonFactory, AbsoluteTerrainHeightBrushTool absoluteTerrainHeightBrushTool, RelativeTerrainHeightBrushTool relativeTerrainHeightBrushTool, NaturalResourceSpawningBrushTool naturalResourceSpawningBrushTool, NaturalResourceRemovalBrushTool naturalResourceRemovalBrushTool) {
+            _toolButtonFactory = toolButtonFactory;
+            _absoluteTerrainHeightBrushTool = absoluteTerrainHeightBrushTool;
+            _relativeTerrainHeightBrushTool = relativeTerrainHeightBrushTool;
+            _naturalResourceSpawningBrushTool = naturalResourceSpawningBrushTool;
+            _naturalResourceRemovalBrushTool = naturalResourceRemovalBrushTool;
+        }
+
+        public IEnumerable<BottomBarElement> GetElements() {
+            ToolButton absoluteHeightToolButton = _toolButtonFactory.CreateGrouplessBlue(_absoluteTerrainHeightBrushTool, AbsoluteTerrainHeightBrushImageKey);
+            yield return BottomBarElement.CreateSingleLevel(absoluteHeightToolButton.Root);
+            ToolButton relativeHeightToolButton = _toolButtonFactory.CreateGrouplessBlue(_relativeTerrainHeightBrushTool, RelativeTerrainHeightBrushImageKey);
+            yield return BottomBarElement.CreateSingleLevel(relativeHeightToolButton.Root);
+            ToolButton resourceSpawningToolButton = _toolButtonFactory.CreateGrouplessBlue(_naturalResourceSpawningBrushTool, NaturalResourcesSpawningImageKey);
+            yield return BottomBarElement.CreateSingleLevel(resourceSpawningToolButton.Root);
+            ToolButton resourceRemovalToolButton = _toolButtonFactory.CreateGrouplessBlue(_naturalResourceRemovalBrushTool, NaturalResourcesRemovalImageKey);
+            yield return BottomBarElement.CreateSingleLevel(resourceRemovalToolButton.Root);
+        }
+    }
+}
